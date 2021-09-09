a63051ef59d2a9cfa783a000cf584c7efad8c8f8
BeommoGu <86589565+Gubeommo@users.noreply.github.com>
2021-09-09 21:51:32 +0900

    Delete Leve1 directory

--- Leve1/K번째수/Py_Solution.py
+++
@@ -1,7 +0,0 @@
-def solution(array, commands):
-    result =[]
-    for i in range(len(commands)):
-        a = array[commands[i][0]-1:commands[i][1]]
-        a.sort()
-        result.append(a[commands[i][2]-1])
-    return result
--- Leve1/예산/Py_Solution.py
+++
@@ -1,9 +0,0 @@
-def solution(d, budget):
-    d.sort()
-    c=0
-    for i in d:
-        if budget-i < 0:
-            break
-        budget -=  i
-        c +=1   
-    return c
